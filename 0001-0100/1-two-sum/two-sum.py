45c563b8f19615fbd0e8343d15f9db3f4a0ab057
fury93 <www.fanreala@gmail.com>
2026-04-27 02:01:22 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (20.60%) - LeetSync

---
+++ 0001-0100/1-two-sum/two-sum.py
@@ -0,0 +1,31 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = dict() # pairs {value: index}
+        
+        for index, value in enumerate(nums):
+            pairValue = target - value
+            if pairValue in seen:
+                return [seen[pairValue], index]
+            seen[value] = index
+
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        nums = sorted((value, index) for index, value in enumerate(nums))
+        l, r = 0, len(nums)-1
+
+        while l < r:
+            sm = nums[l][0] + nums[r][0]
+            if sm > target:
+                r -= 1
+            elif sm < target:
+                l +=1
+            else:
+                break
+
+        return [nums[l][1], nums[r][1]]
+
+    def twoSum3(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
