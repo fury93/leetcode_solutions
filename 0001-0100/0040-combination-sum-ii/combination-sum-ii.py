98c2dbc31e0fd6b39b162cc9a38c6fb91dc42219
fury93 <www.fanreala@gmail.com>
2026-04-30 22:39:15 +0300

    Time: 23 ms (40.71%) | Memory: 19.5 MB (42.60%) - LeetSync

--- 0001-0100/0040-combination-sum-ii/combination-sum-ii.py
+++ 0001-0100/0040-combination-sum-ii/combination-sum-ii.py
@@ -2,24 +2,21 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-        def backtrack(comb, i):
-            comb_sum = sum(comb)
-            
-            if comb_sum > target:
-                return
-            elif comb_sum == target:
+        
+        def backtrack(comb, start, remain):
+            if remain == 0:
                 res.append(comb.copy())
                 return
+            elif remain < 0:
+                return
 
-            for j in range(i, len(candidates)):
-                if j != i and candidates[j] == candidates[j-1]:
+            for j in range(start, len(candidates)):
+                if j > start and candidates[j] == candidates[j-1]:
                     continue
-                if comb_sum + candidates[j] > target:
-                    break
                 comb.append(candidates[j])
-                backtrack(comb, j+1)
+                backtrack(comb, j + 1, remain - candidates[j])
                 comb.pop()
         
-        backtrack([], 0)
+        backtrack([], 0, target)
 
         return res
