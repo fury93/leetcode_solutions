84cfc71231a15bf0ec243b7f673f2ed4ce33ccc5
fury93 <www.fanreala@gmail.com>
2026-05-03 00:53:52 +0300

    Time: 11 ms (5.49%) | Memory: 19.4 MB (50.74%) - LeetSync

--- 0001-0100/0067-add-binary/add-binary.py
+++ 0001-0100/0067-add-binary/add-binary.py
@@ -1,14 +1,16 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res = ""
-        carry = 0
-        max_len = max(len(a), len(b))+1
+        res, carry = [], 0
         
-        for i in range(1, max_len):
-            d1 = a[-i] if i <= len(a) else 0
-            d2 = b[-i] if i <= len(b) else 0
+        for i in range(max(len(a), len(b))):
+            print(a, b, ~i)
+            d1 = a[~i] if i < len(a) else 0
+            d2 = b[~i] if i < len(b) else 0
             carry, d3 = divmod(int(d1) + int(d2) + carry, 2)
-            res = str(d3) + res
+            res.append(str(d3))
 
-        return str(carry) + res if carry else res
+        if carry:
+            res.append(str(carry))
+        
+        return ''.join(res[::-1])
             
