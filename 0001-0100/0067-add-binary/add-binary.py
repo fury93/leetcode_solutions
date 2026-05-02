267bf232d9ce85950e0c40b6f219b5877df494b2
fury93 <www.fanreala@gmail.com>
2026-05-03 00:55:12 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.16%) - LeetSync

--- 0001-0100/0067-add-binary/add-binary.py
+++ 0001-0100/0067-add-binary/add-binary.py
@@ -1,9 +1,12 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        a, b = int(a, 2), int(b, 2)
+        return bin(a + b)[2:]
+
+    def addBinary2(self, a: str, b: str) -> str:
         res, carry = [], 0
         
         for i in range(max(len(a), len(b))):
-            print(a, b, ~i)
             d1 = a[~i] if i < len(a) else 0
             d2 = b[~i] if i < len(b) else 0
             carry, d3 = divmod(int(d1) + int(d2) + carry, 2)
