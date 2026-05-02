a7d17c3835133f175e1a5a6ea91de99eadee34dc
fury93 <www.fanreala@gmail.com>
2026-05-03 02:09:03 +0300

    Time: 9 ms (71.58%) | Memory: 19.6 MB (75.98%) - LeetSync

---
+++ 0901-1000/0989-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        for i in reversed(range(len(num))):
+            k, num[i] = divmod(num[i] + k, 10)
+        
+        return [int(i) for i in str(k)] + num if k else num
+    
+    def addToArrayForm2(self, num: List[int], k: int) -> List[int]:
+        if len(str(k)) > len(num):
+            num, k = list(map(int, str(k))), int(''.join(map(str, num)))
+        
+        carry = 0
+        for i in reversed(range(len(num))):
+            if not k and not carry: break
+            k, remainder = divmod(k, 10)
+            n = num[i] + carry + remainder
+            carry = 0
+            if n <= 9:
+                num[i] = n
+            else:
+                carry, num[i] = divmod(n, 10)
+
+        return num if not carry else [carry] + num
