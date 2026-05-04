8de7e32702c3bd78a007575eac2eb88f9e84cfbd
fury93 <www.fanreala@gmail.com>
2026-05-05 00:51:45 +0300

    Time: 27 ms (99.84%) | Memory: 19.2 MB (28.81%) - LeetSync

--- 0101-0200/0190-reverse-bits/reverse-bits.py
+++ 0101-0200/0190-reverse-bits/reverse-bits.py
@@ -1,7 +1,15 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        res, power = 0, 31
+        while n:
+            res = (n & 1) << power
+            power -= 1
+            n >>= 1
+        return res
+    
+    def reverseBits(self, n: int) -> int:
         res = 0
-        for i in range(32):
+        for _ in range(32):
             res = res << 1 | n & 1
             n >>= 1
         return res
