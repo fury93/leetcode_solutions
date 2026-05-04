ebc166afe548b3cc968813e7be6bf9b8e39921cd
fury93 <www.fanreala@gmail.com>
2026-05-05 01:24:21 +0300

    Time: 40 ms (87.83%) | Memory: 19.3 MB (28.81%) - LeetSync

--- 0101-0200/0190-reverse-bits/reverse-bits.py
+++ 0101-0200/0190-reverse-bits/reverse-bits.py
@@ -2,14 +2,39 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res, power = 0, 31
         while n:
-            res = (n & 1) << power
+            res |= (n & 1) << power
             power -= 1
             n >>= 1
         return res
     
-    def reverseBits(self, n: int) -> int:
+    def reverseBits2(self, n: int) -> int:
         res = 0
         for _ in range(32):
             res = res << 1 | n & 1
             n >>= 1
         return res
+
+# Byte by Byte with Memoization
+class Solution2:
+    def reverseBits(self, n: int) -> int:
+        ret, power = 0, 24
+        while n:
+            ret += self.reverseByte(n & 0xFF) << power
+            n = n >> 8
+            power -= 8
+        return ret
+
+    # memoization with decorator
+    @functools.lru_cache(maxsize=256)
+    def reverseByte(self, byte):
+        return (byte * 0x0202020202 & 0x010884422010) % 1023
+
+# Mask and Shift
+class Solution3:
+    def reverseBits(self, n: int) -> int:
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xFF00FF00) >> 8) | ((n & 0x00FF00FF) << 8)
+        n = ((n & 0xF0F0F0F0) >> 4) | ((n & 0x0F0F0F0F) << 4)
+        n = ((n & 0xCCCCCCCC) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xAAAAAAAA) >> 1) | ((n & 0x55555555) << 1)
+        return n
