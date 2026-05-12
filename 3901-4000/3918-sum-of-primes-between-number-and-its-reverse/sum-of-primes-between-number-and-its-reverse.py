c7712b94acd08151524fe02fe450eae4d6a0acd2
fury93 <www.fanreala@gmail.com>
2026-05-12 18:09:41 +0300

    Time: 16 ms (92.83%) | Memory: 19.3 MB (77.56%) - LeetSync

--- 3901-4000/3918-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.py
+++ 3901-4000/3918-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.py
@@ -3,9 +3,16 @@ class Solution:
         nr = int(str(n)[::-1])
         start, end = min(n, nr), max(n, nr)
         
-        primes = bytearray([True]) * (end + 1)
-        primes[0] = primes[1] = False
-        for p in range(2, math.isqrt(end) + 1):
-            primes[p*p : end+1 : p] = bytearray((end - p*p) // p + 1)
+        def getPrimes(n):
+            primes = bytearray([True]) * (n + 1)
+            primes[0] = primes[1] = False
+            for p in range(2, math.isqrt(n) + 1):
+                primes[p*p : n+1 : p] = bytearray((n - p*p) // p + 1)
+            
+            return [p for p, isPrime in enumerate(primes) if isPrime]
 
-        return sum(v for v, isPrime in enumerate(primes) if isPrime and start <= v <= end)
+        primes = getPrimes(end)
+        l = bisect_left(primes, start)
+        r = bisect_right(primes, end)
+        
+        return sum(primes[l:r])
