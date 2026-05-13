49914154ce46d06ce19950dd75bd103a16565683
fury93 <www.fanreala@gmail.com>
2026-05-13 12:48:16 +0300

    Time: 131 ms (98.46%) | Memory: 31.3 MB (30.77%) - LeetSync

--- 1601-1700/1674-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
+++ 1601-1700/1674-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
@@ -1,6 +1,30 @@
 class Solution:
     def minMoves(self, nums: List[int], limit: int) -> int:
         n = len(nums)
+        
+        deltas = [0] * ((limit << 1) + 2)
+
+        lp = 0
+        rp = n - 1
+        while lp < rp:
+            left, right = nums[lp], nums[rp]
+            if left <= right:
+                small, large = left, right
+            else:
+                small, large = right, left
+            
+            deltas[small + 1] -= 1
+            deltas[small + large] -= 1
+            deltas[small + large + 1] += 1
+            deltas[large + limit + 1] += 1
+
+            lp += 1
+            rp -= 1
+        return n + min(accumulate(deltas))
+        
+class Solution2:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
         sum_count = Counter()
         min_arr = []
         max_arr = []
