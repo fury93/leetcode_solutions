d486e2a498e365078d76c63cc5fcce5c77b696e5
fury93 <www.fanreala@gmail.com>
2026-06-16 14:40:20 +0300

    Time: 3 ms (81.58%) | Memory: 23.3 MB (72.37%) - LeetSync

--- 3601-3700/3612-process-string-with-special-operations-i/process-string-with-special-operations-i.py
+++ 3601-3700/3612-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -1,5 +1,19 @@
 class Solution:
     def processStr(self, s: str) -> str:
+        ans = ""
+        for i in s:
+            if i.islower():
+                ans += i
+            elif i == '*':
+                if ans:
+                    ans = ans[:-1]            
+            elif i == '#':
+                ans += ans
+            elif i == '%':
+                ans = ans[::-1]
+        return ans
+
+    def processStr2(self, s: str) -> str:
         result = []
         for ch in s:
             if ch == "*":
