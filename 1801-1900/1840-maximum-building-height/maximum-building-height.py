1dded880e26502d8d1fb7aef4cf5ab5798422b8c
fury93 <www.fanreala@gmail.com>
2026-06-20 14:07:51 +0300

    Time: 144 ms (97.73%) | Memory: 46.8 MB (9.09%) - LeetSync

--- 1801-1900/1840-maximum-building-height/maximum-building-height.py
+++ 1801-1900/1840-maximum-building-height/maximum-building-height.py
@@ -1,4 +1,13 @@
 class Solution:
+    def maxBuilding(self, n: int, re: List[List[int]]) -> int:
+        re.sort(key=lambda i:i[0]+i[1])
+        ans, b = 0, [1,0]
+        for r in re:
+            ans = max(ans,(min(2*n-b[0]+b[1],r[0]+r[1])-(b[0]-b[1]))//2)
+            if b[0]-b[1] < r[0]-r[1]: b = [r[0],r[1]]
+        return max(ans,n-b[0]+b[1])
+        
+class Solution2:
     def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
         r = restrictions
         # Add restriction (1, 0)
