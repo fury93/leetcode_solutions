d95af7acf300382640a3fc73c902f715b5ceb0b6
fury93 <www.fanreala@gmail.com>
2026-06-26 04:27:54 +0300

    Time: 179 ms (61.26%) | Memory: 47.3 MB (7.21%) - LeetSync

--- 3701-3800/3739-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.py
+++ 3701-3800/3739-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.py
@@ -1,6 +1,26 @@
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
+
+        freq = defaultdict(int)
+        acc = defaultdict(int)
+        freq[0], acc[0] = 1, 1
+
+        bal = 0
+        res = 0
+
+        for num in nums:
+            if num == target: bal += 1
+            else: bal -= 1
+            
+            freq[bal] += 1
+            acc[bal] = acc[bal - 1] + freq[bal]
+            res += acc[bal - 1]
+
+        return res
+        
+    def countMajoritySubarrays2(self, nums: List[int], target: int) -> int:
+        n = len(nums)
         pre = [0] * (n * 2 + 1)
         pre[n] = 1
         cnt = n
