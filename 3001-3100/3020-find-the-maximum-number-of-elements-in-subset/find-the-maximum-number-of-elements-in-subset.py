0fc39da0f43e1c335dfccdd7661ae509c643e47e
fury93 <www.fanreala@gmail.com>
2026-06-27 13:41:45 +0300

    Time: 90 ms (95.28%) | Memory: 31.4 MB (89.76%) - LeetSync

--- 3001-3100/3020-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
+++ 3001-3100/3020-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.py
@@ -1,10 +1,21 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
         cnt = Counter(nums)
+        ans = cnt[1] - (cnt[1] % 2 ^ 1)
+        del cnt[1]
+        for x in cnt:
+            t = 0
+            while cnt[x] > 1:
+                x = x * x
+                t += 2
+            t += 1 if cnt[x] else -1
+            ans = max(ans, t)
+        return ans
+
+    def maximumLength2(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
 
         one_cnt = cnt.get(1, 0)
-
-        # ans is at least the number of occurrences of 1, rounded down to an odd number
         ans = one_cnt if one_cnt % 2 else one_cnt - 1
 
         cnt.pop(1, None)
