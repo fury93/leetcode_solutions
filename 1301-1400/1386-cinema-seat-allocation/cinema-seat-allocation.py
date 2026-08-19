a889469d7ec46a39957a0a787b43ae4a71f063ef
fury93 <www.fanreala@gmail.com>
2026-08-19 12:22:18 +0300

    Time: 38 ms (59.90%) | Memory: 23.2 MB (38.56%) - LeetSync

--- 1301-1400/1386-cinema-seat-allocation/cinema-seat-allocation.py
+++ 1301-1400/1386-cinema-seat-allocation/cinema-seat-allocation.py
@@ -1,5 +1,25 @@
 class Solution:
-    def maxNumberOfFamilies(
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        seats = defaultdict(set)
+
+        for row, seat in reservedSeats:
+            if seat in [2, 3, 4, 5]:
+                seats[row].add(0)
+            if seat in [4, 5, 6, 7]:
+                seats[row].add(1)
+            if seat in [6, 7, 8, 9]:
+                seats[row].add(2)
+
+        res = 2 * n
+        for i in seats:
+            if len(seats[i]) == 3:
+                res -= 2
+            else:
+                res -= 1
+
+        return res
+        
+    def maxNumberOfFamilies2(
         self, n: int, reservedSeats: List[List[int]]
     ) -> int:
         left, middle, right = 0b11110000, 0b11000011, 0b00001111
