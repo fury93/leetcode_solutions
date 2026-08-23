9c7ddd0193feb71fd73bb11862fe9720931180f0
fury93 <www.fanreala@gmail.com>
2026-08-23 13:03:43 +0300

    Time: 37 ms (95.29%) | Memory: 20.8 MB (6.47%) - LeetSync

--- 1901-2000/1927-sum-game/sum-game.py
+++ 1901-2000/1927-sum-game/sum-game.py
@@ -1,5 +1,10 @@
 class Solution:
     def sumGame(self, num: str) -> bool:
+        n = len(num) // 2
+        num = [9 if ch == '?' else 2 * int(ch) for ch in num]                                                
+        return  sum(num[:n]) != sum(num[n:])
+
+    def sumGame2(self, num: str) -> bool:
         n = len(num)
 
         def get(s: str) -> (int, int):
