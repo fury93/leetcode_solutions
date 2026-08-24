13d4f2bc97452aaaca604af3ac7ce32acbe16f90
fury93 <www.fanreala@gmail.com>
2026-08-24 13:47:29 +0300

    Time: 678 ms (57.99%) | Memory: 33 MB (75.15%) - LeetSync

--- 1801-1900/1872-stone-game-viii/stone-game-viii.py
+++ 1801-1900/1872-stone-game-viii/stone-game-viii.py
@@ -1,5 +1,14 @@
 class Solution:
-    def stoneGameVIII(self, stones: List[int]) -> int:
+    def stoneGameVIII(self, stones: list[int]) -> int:
+        for index in range(1, len(stones)):
+            stones[index] += stones[index - 1]
+
+        best = stones[-1]
+        for index in range(len(stones) - 2, 0, -1):
+            best = max(best, stones[index] - best)
+        return best
+
+    def stoneGameVIII2(self, stones: List[int]) -> int:
         n = len(stones)
         pre = list(accumulate(stones))
         f = [0] * n
