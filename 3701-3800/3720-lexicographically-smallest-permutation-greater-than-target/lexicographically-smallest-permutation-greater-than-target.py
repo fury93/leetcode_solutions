d4fc922d08f7dca725b8a63f01e9894f22d3a932
fury93 <www.fanreala@gmail.com>
2026-08-27 12:50:28 +0300

    Time: 3 ms (97.37%) | Memory: 19.4 MB (71.05%) - LeetSync

--- 3701-3800/3720-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
+++ 3701-3800/3720-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
@@ -1,5 +1,39 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
+        n = len(s)
+        cnt = [0] * 26
+        for ch in s:
+            cnt[ord(ch) - 97] += 1
+
+        # Consume as much of target's prefix as the multiset allows.
+        p = 0
+        while p < n:
+            c = ord(target[p]) - 97
+            if cnt[c] == 0:
+                break
+            cnt[c] -= 1
+            p += 1
+
+        i = p
+        while i >= 0:
+            if i < n:
+                t = ord(target[i]) - 97
+                pick = -1
+                for c in range(t + 1, 26):
+                    if cnt[c] > 0:
+                        pick = c
+                        break
+                if pick >= 0:
+                    cnt[pick] -= 1
+                    tail = ''.join(chr(97 + c) * cnt[c] for c in range(26))
+                    cnt[pick] += 1
+                    return target[:i] + chr(97 + pick) + tail
+            i -= 1
+            if i >= 0:
+                cnt[ord(target[i]) - 97] += 1
+        return ""
+        
+    def lexGreaterPermutation2(self, s: str, target: str) -> str:
         cnt = [0] * 26
         for i in range(len(s)):
             cnt[ord(s[i]) - ord("a")] += 1
